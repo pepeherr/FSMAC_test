8c435ce452e6184b09a036dda68c02287717ec73
pepeherr <pepeherr@gmail.com>
2026-02-25 23:35:50 +0000

    finalizado necesidades-acciones

---
+++ config/initializers/need_action_proposals/decidim_create_proposal_extension.rb
@@ -0,0 +1,19 @@
+# config/initializers/decidim_create_proposal_extension.rb
+
+Rails.application.config.to_prepare do
+  next unless defined?(Decidim::Proposals::CreateProposal)
+
+  Decidim::Proposals::CreateProposal.class_eval do
+    alias_method :original_create_proposal, :create_proposal
+
+    private
+
+    def create_proposal
+      original_create_proposal
+      # Solo para componente Acciones
+      if form.component.fsmac_action? && form.parent_objective_id.present?
+        proposal.update_column(:parent_objective_id, form.parent_objective_id)
+      end
+    end
+  end
+end
