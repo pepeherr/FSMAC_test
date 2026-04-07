31e03797594c077ad5d0ca6c2a69494ef1052735
pepeherr <95982159+pepeherr@users.noreply.github.com>
2026-04-07 11:27:34 +0200

    Dev (#22)
    * Completado listado de propuestas junto a las acciones en página f/37/proposals
    
    * Creado listado de Necesidades bajo Conflictos
    	new file:   app/models/concerns/fsmac/fsmac_methods.rb
    	new file:   app/views/decidim/participatory_processes/participatory_processes/_collection.html.erb
    	new file:   config/initializers/process/fsmac_process_extension.rb
    	new file:   x_scripts/table_conf_to_csv.rb
    	new file:   x_scripts/user_apply_roles.rb
    	new file:   x_scripts/user_find_roles.rb
    
    * new file:   app/controllers/decidim/tree_controller.rb
    	new file:   app/views/decidim/tree/_global_proposals.html.erb
    	new file:   app/views/decidim/tree/_node_assembly.html.erb
    	new file:   app/views/decidim/tree/_node_assembly.html.erb.old
    	new file:   app/views/decidim/tree/_proposals_list.html.erb
    	new file:   app/views/decidim/tree/index.html.erb
    	new file:   config/initializers/process/participatory_process.rb
    	new file:   config/initializers/tree/tree_menu.rb
    	new file:   config/locales/tree_es.yml
    	new file:   x_scripts/fsmac_arbol.rb
    	new file:   x_scripts/fsmac_arbol__v_2.rb
    	new file:   x_scripts/fsmac_arbol_v_1.rb
    	new file:   x_scripts/fsmac_status.rb
    
    * casi nada
    
    * terminado menu arbol
    
    * Changes to be committed:
    	new file:   app/controllers/concerns/action_helper.rb
    	new file:   app/views/decidim/participatory_processes/participatory_processes/_collection.html.erb.save
    	modified:   app/views/decidim/proposals/proposals/_edit_form_fields.html.erb
    	new file:   app/views/decidim/proposals/proposals/readme.md
    	renamed:    app/views/decidim/proposals/proposals/show.html.erb -> app/views/decidim/proposals/proposals/show.html.erb.save
    	modified:   app/views/layouts/decidim/header/_main_links_desktop.html.erb
    	modified:   config/initializers/need_action_proposals/fsmac_component_extension.rb
    	modified:   config/initializers/tree/tree_menu.rb
    	deleted:    config/initializers/tree/tree_menu.rb.save
    	modified:   config/locales/overrides_es.yml
    	new file:   x_howto/Preguntas.md

---
+++ app/views/decidim/proposals/proposals/readme.md
@@ -0,0 +1,4 @@
+## Proposito de app/views/decidim/proposals/proposals/show.html.erb
+Esta vista sobreescribe la vista original para añadir un listado de propuestas en la vista proposals/id
+Sería funcional cuando se requisiese propuestas encadenadas de la forma o Propuestas -> Acción
+Para dejar el código sin eliminarlo, lo que hemos hecho ahora es simplemente renombrar show.html.erb como show.html.erb.save
--- x_scripts/test_menu.rb
+++ x_scripts/test_menu.rb
@@ -1,16 +1,24 @@
 # scripts/test_menu.rb
-puts "=" * 60
+puts "=" * 21
 puts "VERIFICACIÓN DE MENÚS"
-puts "=" * 60
+puts "=" * 21
 
-menus_a_probar = [:navbar, :user_menu, :menu, :main_menu, :admin_menu]
+menus_a_probar = Decidim::MenuRegistry.instance_values["all"].keys
 
 menus_a_probar.each do |menu_name|
-  begin
-    menu_registry = Decidim::MenuRegistry.find(menu_name)
-    puts "✅ :#{menu_name} - ENCONTRADO"
-    puts "   Configuraciones: #{menu_registry.configurations.count}"
-  rescue
-    puts "❌ :#{menu_name} - NO ENCONTRADO"
+  menu_registry = Decidim::MenuRegistry.find(menu_name)
+  puts "Menú: :#{menu_name}"
+  puts "   Configuraciones: #{menu_registry.configurations.count}"
+  # Cada menú es un objeto que contiene los items registrados
+  if menu.respond_to?("configurations")
+    menu.items.each do |item|
+      puts "  - #{item.name}: #{item.url}"
+      puts "    Active: #{item.active?}"
+      puts "    Position: #{item.position}"
+      puts "    Options: #{item.options}"
+    end
   end
+  puts "\n"
+rescue StandardError
+  puts " :#{menu_name} - NO ENCONTRADO O SIN CONFIGURACIONES"
 end
